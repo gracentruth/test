c2f64b966526fc34fe7c997351323c6a43409562
Kim eunjin <21900174@handong.edu>
2020-04-25 02:15:50 +0900

    update

--- manager.c
+++ manager.c
@@ -40,7 +40,12 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	for(int i=0;i<count;i++){
+if(p[i].price!=-1){
+fprintf(fp,"%d %d %s\n",p[i].weight,p[i].price,p[i].name);
+}
+}
+
 	
 	fclose(fp);
 	printf("저장됨!\n");
@@ -53,10 +58,19 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	fp=fopen("product.txt","rt");
+		if(fp==NULL){
+			printf("=>파일 없음\n");
+			return 0;
+		}
+	for(;;count++){
+		fscanf(fp,"%d %d %[^\n]s",&p[count].weight,&p[count].price,p[count].name);
+	if(feof(fp))
+	break;
+	}
 
 
-
-
+	fclose(fp);
 	printf("=> 로딩 성공!\n");
 	return count;
 }
